68f6b2e30f2834de45d9b632ba138290e822b5bd
enzoreyes11 <reyes.enzo1985@gmail.com>
2025-10-27 20:52:28 +0000

    [ADD] New web3forms key and site navigation
    - Added the client's web3forms access key
    - Added IG to the header navigation
    - footer links open in a new page

--- site/src/navigation.ts
+++ site/src/navigation.ts
@@ -16,9 +16,16 @@ export const headerData = {
       href: getPermalink('/contacto'),
     },
   ],
-  actions: [{ text: 'Whatsapp', 
+  actions: [{ text: 'WhatsApp', 
     icon: 'tabler:brand-whatsapp',
-    href: `https://wa.me/${site_content.contact.whatsapp.number}?text=${encodeURIComponent(site_content.contact.whatsapp.message)}`, target: '_blank' }],
+    href: `https://wa.me/${site_content.contact.whatsapp.number}?text=${encodeURIComponent(site_content.contact.whatsapp.message)}`,
+    target: '_blank' }, {
+
+    text: 'Instagram',
+    icon: 'tabler:brand-instagram',
+    href: site_content.social.instagram,
+    target: '_blank' },
+   ],
 };
 
 export const footerData = {
@@ -46,10 +53,10 @@ export const footerData = {
   ],
   secondaryLinks: [],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: site_content.social.twitter },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: site_content.social.instagram },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: site_content.social.facebook },
-    { ariaLabel: 'WhatsApp', icon: 'tabler:brand-whatsapp', href: `https://wa.me/${site_content.contact.whatsapp.number}?text=${encodeURIComponent(site_content.contact.whatsapp.message)}`},
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: site_content.social.twitter, target: '_blank' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: site_content.social.instagram, target: '_blank'  },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: site_content.social.facebook, target: '_blank'  },
+    { ariaLabel: 'WhatsApp', icon: 'tabler:brand-whatsapp', href: `https://wa.me/${site_content.contact.whatsapp.number}?text=${encodeURIComponent(site_content.contact.whatsapp.message)}`, target: '_blank' },
   ],
   footNote: `
     Made by <a class="text-blue-600 underline dark:text-muted" href="http://www.enzoreyes.dev"> Enzo Reyes</a> · All rights reserved.
